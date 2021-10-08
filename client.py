4143667a8a995afec57b919a42241525a333c987
clément-devevey <poubelledekikoo@gmail.com>
2021-10-08 19:35:55 +0200

    Worker now using while(1) to wait for data

--- client.py
+++ client.py
@@ -4,9 +4,10 @@ import boto3
 sqs = boto3.resource('sqs')
 
 # Create the queue. This returns an SQS.Queue instance
-#queue = sqs.create_queue(QueueName='requestQueue')
+
 inputt = input("Entrez au maximum 10 valeurs separees par des espaces")
 
+sqs.create_queue(QueueName = 'requestQueue')
 queue = sqs.get_queue_by_name(QueueName='requestQueue')
 
 queue.send_message(MessageBody='boto3', MessageAttributes={
--- worker.py
+++ worker.py
@@ -1,23 +1,29 @@
 import boto3
+import time
 
 # Get the service resource
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='requestQueue')
 
-for message in queue.receive_messages(MessageAttributeNames=['Clement']):
-    # Get the custom author message attribute if it was set'
-    if message.message_attributes is not None:
-        mes_int = message.message_attributes.get('Clement').get('StringValue')
-        mes_int_split = mes_int.split()
-        
-    # Let the queue know that the message is processed
-    message.delete()
+while (1):
+    print("Waiting for data")
+    time.sleep(1)
     queue.reload()
-    moyenne = 0
-    for i in range (len(mes_int_split)):
-        moyenne = moyenne + int(mes_int_split[i])
+    for message in queue.receive_messages(MessageAttributeNames=['Clement']):
+        # Get the custom author message attribute if it was set'
+        if message.message_attributes is not None:
+            print("Data Receveid !")
+            mes_int = message.message_attributes.get('Clement').get('StringValue')
+            mes_int_split = mes_int.split()
+            
+        # Let the queue know that the message is processed
+        message.delete()
+        queue.reload()
+        moyenne = 0
+        for i in range (len(mes_int_split)):
+            moyenne = moyenne + int(mes_int_split[i])
+            
+        moyenne = moyenne / len(mes_int_split)
+        print("Moyenne = ", moyenne)
         
-    moyenne = moyenne / len(mes_int_split)
-    print("Moyenne = ", moyenne)
-    
-    
+        
